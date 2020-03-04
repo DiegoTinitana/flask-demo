bcab69af93b597ce53073a101e6babf84849a766
roberto <alexandro.ramr777@gmail.com>
2020-03-03 21:35:51 -0500

    [UPDATE] add query parameters

--- api-demo/app.py
+++ api-demo/app.py
@@ -1,18 +1,40 @@
 from flask import Flask, jsonify
-from flask import request
+from flask import request, make_response
 
 app = Flask(__name__)
 
 movies_list = [
     {
         "name": "The Shawshank Redemption",
-        "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
-        "genres": ["Drama"]
+        "casts": [
+            "Tim Robbins",
+            "Morgan Freeman",
+            "Bob Gunton",
+            "William Sadler"
+        ],
+        "genres": ["Drama"],
+        "budget": 25000000
     },
     {
        "name": "The Godfather ",
-       "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
-       "genres": ["Crime", "Drama"]
+       "casts": [
+           "Marlon Brando",
+           "Al Pacino",
+           "James Caan",
+           "Diane Keaton"
+        ],
+       "genres": ["Crime", "Drama"],
+       "budget": 6000000
+    },
+    {
+       "name": "The Avengers",
+       "casts": [
+           "Robert Downey Jr",
+           "Scarlett Johansson",
+           "Chris Evans"
+        ],
+       "genres": ["Fiction", "Action"],
+       "budget": 220000000
     }
 ]
 
@@ -20,12 +42,29 @@ movies_list = [
 @app.route('/')
 @app.route('/movies')
 def movies():
+    if request.method == "GET":
+        print("GET method listened.")
+
+    if request.args:
+        print("Args present!")
+
+        if "budget_lt" in request.args:
+            budget_max = int(request.args.get('budget_lt'))
+            movies_list_filtered = [
+                movie for movie in movies_list
+                if movie['budget'] < budget_max
+            ]
+            return jsonify(movies_list_filtered), 200
+
     return jsonify(movies_list), 200
 
 
 @app.route('/movies', methods=['POST'])
 def add_movie():
-    movie = request.get_json()
+    if request.method == "POST":
+        print("POST method listened.")
+
+    movie = request.get_json(force=True)
     movies_list.append(movie)
     return {'id': len(movies_list)}, 201
 
@@ -40,7 +79,7 @@ def update_movie(index):
 @app.route('/movies/<int:index>', methods=['DELETE'])
 def delete_movie(index):
     movies_list.pop(index)
-    return jsonify(movies_list), 204
+    return {}, 204
 
 
 if __name__ == '__main__':
